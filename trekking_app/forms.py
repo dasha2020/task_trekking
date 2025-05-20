4f1f890a2cd40766441e65ffdf91d9888a0de8eb
dasha2020 <darenddhaker128675@gmail.com>
2025-05-20 19:28:16 +0300

    Add Login and Registration forms

--- trekking_app/forms.py
+++ trekking_app/forms.py
@@ -45,3 +45,41 @@ class TaskForm(forms.Form):
             'type': 'datetime-local'
         })
     )
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
+
+class CustomUserCreationForm(forms.Form):
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        help_text="Password must be at least 8 characters and contain numbers, symbols and letters.")
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        help_text="Please, confirm your password.")
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password")
+        validate_password(password1)
+
+        return password1
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("confirm_password")
+        
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self):
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],
+            email=self.cleaned_data['email'],
+            password=self.cleaned_data['password']
+        )
+        return user 
